d1b15dced912d50744470efcbe4eb09048d55694
Jon Wretlind <jonw@beyondbluemedia.com>
2019-02-13 08:42:10 -0700

    Updated SectionFeatures 2/12/19

--- src/components/SectionFeatures/SectionFeatures.js
+++ src/components/SectionFeatures/SectionFeatures.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-
 import css from './SectionFeatures.css';
 
-const SectionFeatures = props => {
-  const { options, selectedOptions } = props;
-  return (
-    <div className={css.sectionFeatures}>
-      <h1 className={css.title}>
-        <FormattedMessage id="SectionFeatures.featuresTitle" />
-      </h1>
-     
-    </div>
-  );
-};
+const sharetribeSdk = require('sharetribe-flex-sdk');
 
-export default SectionFeatures;
+// Create new SDK instance
+const sdk = sharetribeSdk.createInstance({
+  clientId: 'd9b92f0a-04e0-4a68-82e5-e3352c09fdeb'
+});
+
+let listingHTML = "";
+
+export default function SectionFeatures() {
+     sdk.listings
+    .query({ per_page: 3 })
+    .then(response => {
+      // Print listing titles
+      response.data.data.forEach(listing => {
+        listingHTML = listingHTML + '<div>' + listing.attributes.title + '</div>';
+      })
+
+      console.log(listingHTML);
+      document.getElementById('myListings').innerHTML = listingHTML;
+    })
+    .catch(response => {
+      // An error occurred
+      console.log(`Request failed with status: ${response.status} ${response.statusText}`);
+    });
+
+    return (
+      <div className={css.sectionFeatures}>
+        <h1 className={css.title}>
+          <FormattedMessage id="SectionFeatures.featuresTitle" />
+        </h1>
+        <div id="myListings">
+        </div>
+      </div>
+    )
+};
